4de9acbd8fd77192c39eef08e9e103950c29de36
htjb <harrytjbevins@yahoo.co.uk>
2024-01-19 17:00:11 +0000

    including nre stuff

--- toy_example_different_priors.py
+++ toy_example_different_priors.py
@@ -6,11 +6,21 @@ from tensionnet.tensionnet import nre
 from tensionnet.robs import run_poly
 from pypolychord.priors import UniformPrior, LogUniformPrior
 import os
+from tensionnet.tensionnet import nre
+import tensorflow as tf
+from scipy.stats import ecdf
 
 def signal_func_gen(freqs):
     def signal(_, parameters):
         amp, nu_0, w = parameters
         return -amp * np.exp(-(freqs-nu_0)**2 / (2*w**2))
+    return signal
+
+def nre_signal_func_gen(freqs):
+    def signal(_, parameters):
+        amp, nu_0, w = parameters
+        return -amp * np.exp(-(freqs-nu_0)**2 / (2*w**2)) + \
+            np.random.normal(0, 0.025, len(freqs))
     return signal
 
 def exp1likelihood(theta):
@@ -29,23 +39,41 @@ def jointlikelihood(theta):
     return exp1likelihood(exp1theta)[0] + exp2likelihood(exp2theta)[0], []
 
 def build_priors(prior_bounds):
-    def signal_poly_prior(cube):
+    def signal_prior(cube):
         theta = np.zeros(4)
-        theta[0] = UniformPrior(**prior_bounds[0])(cube[0]) #amp
-        theta[1] = UniformPrior(**prior_bounds[1])(cube[1]) #nu_0
-        theta[2] = UniformPrior(**prior_bounds[2])(cube[2]) #w
+        theta[0] = UniformPrior(prior_bounds[0][0], prior_bounds[0][1])(cube[0]) #amp
+        theta[1] = UniformPrior(prior_bounds[1][0], prior_bounds[1][1])(cube[1]) #nu_0
+        theta[2] = UniformPrior(prior_bounds[2][0], prior_bounds[2][1])(cube[2]) #w
         theta[3] = LogUniformPrior(0.001, 0.1)(cube[3]) #noise
         return theta
 
     def joint_prior(cube):
         theta = np.zeros(5)
-        theta[0] = UniformPrior(**prior_bounds[0])(cube[0]) #amp
-        theta[1] = UniformPrior(**prior_bounds[1])(cube[1]) #nu_0
-        theta[2] = UniformPrior(**prior_bounds[2])(cube[2]) #w
+        theta[0] = UniformPrior(prior_bounds[0][0], prior_bounds[0][1])(cube[0]) #amp
+        theta[1] = UniformPrior(prior_bounds[1][0], prior_bounds[1][1])(cube[1]) #nu_0
+        theta[2] = UniformPrior(prior_bounds[2][0], prior_bounds[2][1])(cube[2]) #w
         theta[3] = LogUniformPrior(0.001, 0.1)(cube[3]) #exp1 noise
         theta[4] = LogUniformPrior(0.001, 0.1)(cube[4]) #exp2 noise
         return theta
-    return signal_poly_prior, joint_prior
+    return signal_prior, joint_prior
+
+def build_nre_priors(prior_bounds):
+    def signal_prior(n):
+        parameters = np.ones((n, 3))
+        parameters[:, 0] = np.random.uniform(prior_bounds[0][0], prior_bounds[0][1], n) #amp
+        parameters[:, 1] = np.random.uniform(prior_bounds[1][0], prior_bounds[1][1], n) #nu_0
+        parameters[:, 2] = np.random.uniform(prior_bounds[2][0], prior_bounds[2][1], n) #w
+        return parameters
+    return signal_prior
+
+def exp_prior(n):
+    """
+    The way tensionnet is set up it requires some
+    parameters that are unique to each experiment. Here I give an array of
+    zeros because the experimetns are just signal plus noise. Doesn't have
+    any impact on the results.
+    """
+    return np.zeros((n, 2))
 
 
 base = 'toy_chains_priors/'
@@ -57,47 +85,106 @@ exp1_freq = np.linspace(60, 90, 100)
 exp2_freq = np.linspace(80, 120, 100)
 exp1_sf = signal_func_gen(exp1_freq)
 exp2_sf = signal_func_gen(exp2_freq)
+exp1_sf_nre = nre_signal_func_gen(exp1_freq)
+exp2_sf_nre = nre_signal_func_gen(exp2_freq)
 
 true_params = np.array([0.2, 78.0, 10.0])
 
 wide_prior_bounds = np.array([[0.0, 4.0], [60.0, 90.0], [5.0, 40.0]])
-conservative_prior_bounds = np.array([[0.5, 1.0], [70.0, 80.0], [5.0, 15.0]])
-narrow_prior_bounds = np.array([[0.2, 0.3], [76.0, 80.0], [8.0, 12.0]])
+conservative_prior_bounds = np.array([[0.0, 1.0], [70.0, 80.0], [5.0, 15.0]])
+narrow_prior_bounds = np.array([[0.0, 0.3], [76.0, 80.0], [8.0, 12.0]])
 
-prior_sets = [wide_prior_bounds, conservative_prior_bounds, narrow_prior_bounds]
-prior_sets_names = ['wide', 'conservative', 'narrow']
+prior_sets = [wide_prior_bounds, 
+              #conservative_prior_bounds
+              #, narrow_prior_bounds
+              ]
+prior_sets_names = ['wide', 
+                    #'conservative', 
+                    #'narrow'
+                    ]
 Rs = []
+fig, axes = plt.subplots(3, 2, figsize=(5, 10))
 for i, ps in enumerate(prior_sets):
     signal_prior, joint_prior = build_priors(ps)
-    base += prior_sets_names[i] + '/'
-    if not os.path.exists(base):
-        os.mkdir(base)
+    sbase = base + prior_sets_names[i] + '/'
+    if not os.path.exists(sbase):
+        os.mkdir(sbase)
 
     try:
-        exp1_data = np.loadtxt(base + 'exp1_data.txt')
+        exp1_data = np.loadtxt(sbase + 'exp1_data.txt')
     except:
         exp1_data = exp1_sf([None], true_params) \
             + np.random.normal(0, 0.025, 100)
-        np.savetxt(base + 'exp1_data.txt', exp1_data)
+        np.savetxt(sbase + 'exp1_data.txt', exp1_data)
 
-    run_poly(signal_prior, exp1likelihood, base + f'exp1', nlive=1000, RESUME=RESUME)
-    exp1_samples = read_chains(base + f'exp1/test')
+    run_poly(signal_prior, exp1likelihood, sbase + f'exp1', RESUME=RESUME)
+    exp1_samples = read_chains(sbase + f'exp1/test')
 
     try:
-        exp2_data = np.loadtxt(base + f'exp2_data.txt')
+        exp2_data = np.loadtxt(sbase + f'exp2_data.txt')
     except:
         exp2_data = exp2_sf([None], true_params) \
             + np.random.normal(0, 0.025, 100)
-        np.savetxt(base + f'exp2_data.txt', exp2_data)
+        np.savetxt(sbase + f'exp2_data.txt', exp2_data)
 
-    run_poly(joint_prior, jointlikelihood, base + f'joint', RESUME=RESUME, nDims=5)
-    run_poly(signal_prior, exp2likelihood, base + f'exp2', RESUME=RESUME)
+    run_poly(joint_prior, jointlikelihood, sbase + f'joint', RESUME=RESUME, nDims=5)
+    run_poly(signal_prior, exp2likelihood, sbase + f'exp2', RESUME=RESUME)
 
-    exp2_samples = read_chains(base + f'exp2/test')
-    joint_samples = read_chains(base + f'joint/test')
-
-    Rs.append(joint_samples.logZ(1000) - 
-              exp1_samples.logZ(1000) - exp2_samples.logZ(1000))
+    exp2_samples = read_chains(sbase + f'exp2/test')
+    joint_samples = read_chains(sbase + f'joint/test')
 
 
-print(np.mean(Rs, axis=1), np.std(Rs, axis=1))
+    Rs.append((joint_samples.logZ(1000) - 
+              exp1_samples.logZ(1000) - exp2_samples.logZ(1000)).values)
+    
+    nre_signal_prior = build_nre_priors(ps)
+
+    try:
+        nrei = nre.load(sbase + 'model.pkl',
+                exp2_sf_nre, exp1_sf_nre, exp_prior,
+                exp_prior, nre_signal_prior)
+    except:
+        nrei = nre(lr=1e-4)
+        nrei.build_model(len(exp2_freq) + len(exp1_freq), 1, 
+                            [100]*10, 'sigmoid')
+        nrei.build_simulations(exp2_sf_nre, exp1_sf_nre, 
+                               exp_prior, exp_prior, nre_signal_prior, n=100000)
+        model, data_test, labels_test = nrei.training(epochs=1000, batch_size=2000)
+        nrei.save(sbase + 'model.pkl')
+
+    nrei.__call__(iters=5000)
+    r = nrei.r_values
+    mask = np.isfinite(r)
+    sigr = tf.keras.layers.Activation('sigmoid')(r[mask])
+    c = 0
+    good_idx = []
+    for j in range(len(sigr)):
+        if sigr[j] < 0.75:
+            c += 1
+        else:
+            good_idx.append(j)
+
+    print((len(sigr) -c)/len(sigr))
+
+    r = r[good_idx]
+    mask = np.isfinite(r)
+
+    Robs = Rs[-1].mean()
+    errorRs = np.std(Rs[-1])
+
+    axes[i, 0].hist(r[mask], bins=50, density=True)
+    axes[i, 0].set_xlabel(r'\log R')
+    axes[i, 0].axvline(Robs, color='k')
+
+    r  = np.sort(r[mask])
+    c = ecdf(r)
+    axes[i, 1].plot(r, c.cdf.evaluate(r))
+    axes[i, 1].axhline(c.cdf.evaluate(Robs), ls='--',
+                color='r')
+    axes[i, 1].axhspan(c.cdf.evaluate(Robs - errorRs), 
+            c.cdf.evaluate(Robs + errorRs), 
+            alpha=0.1, 
+            color='r')
+
+plt.show()
+
