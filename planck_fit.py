7208c90947c658f79b17f08fb11a208860a0a3d5
htjb <harrytjbevins@yahoo.co.uk>
2024-01-24 09:53:12 +0000

    updating planck fitting code

--- planck_fit.py
+++ planck_fit.py
@@ -1,129 +1,54 @@
-#from mpi4py import MPI
-import numpy as np
-#from tensionnet.robs import run_poly
 from pypolychord.settings import PolyChordSettings
-from pypolychord.priors import UniformPrior, LogUniformPrior
-import camb
-import matplotlib.pyplot as plt
-from scipy.stats import chi2
 import pypolychord
-
+from cmblike.data import get_data
+from cmblike.noise import planck_noise
+from cmblike.cmb import CMB
 
 nDims = 6
 nDerived = 0
 
-def load_planck():
+p, l = get_data(base_dir='cosmology-data/').get_planck()
+planck_noise = planck_noise(l).calculate_noise()
 
-    """
-    Function to load in the planck power spectrum data.
+cmbs = CMB()
 
-    Returns
-    -------
-    p: power spectrum
-    ps: the error on the power spectrum
-    l_real: the multipoles
-    """
+likelihood = cmbs.get_likelihood(p, l, noise=planck_noise)
+prior = cmbs.prior
 
-    tt = np.loadtxt('TT_power_spec.txt', delimiter=',', dtype=str)
-
-    l_real, p, ps, ns = [], [], [], []
-    for i in range(len(tt)):
-        if tt[i][0] == 'Planck binned      ':
-            l_real.append(tt[i][2].astype('float')) # ell
-            p.append(tt[i][4].astype('float')) # power spectrum
-            ps.append(tt[i][6].astype('float')) # positive error
-            ns.append(tt[i][5].astype('float')) # negative error
-    p, ps, l_real = np.array(p), np.array(ps), np.array(l_real)
-    return p, ps, l_real
-
-p, _, l_real = load_planck()
-p *= (2*np.pi)/(l_real*(l_real+1)) # convert to C_l
-
-def prior(cube):
-    theta = np.zeros(len(cube))
-    theta[0] = UniformPrior(0.01, 0.085)(cube[0]) # omegabh2
-    theta[1] = UniformPrior(0.08, 0.21)(cube[1]) # omegach2
-    theta[2] = UniformPrior(0.97, 1.5)(cube[2]) # 100*thetaMC
-    theta[3] = UniformPrior(0.01, 0.16)(cube[3]) # tau
-    theta[4] = UniformPrior(0.8, 1.2)(cube[4]) # ns
-    theta[5] = UniformPrior(2.6, 3.8)(cube[5]) # log(10^10*As)
-    return theta
-
-# from montepython https://github.com/brinckmann/montepython_public/blob/3.6/montepython/likelihoods/fake_planck_bluebook/fake_planck_bluebook.data
-theta_planck = np.array([10, 7.1, 5.0]) # in arcmin
-sigma_T = np.array([68.1, 42.6, 65.4]) # in muK arcmin
-
-theta_planck *= np.array([np.pi/60/180])
-sigma_T *= np.array([np.pi/60/180])
-
-
-nis = []
-for i in range(len(sigma_T)):
-    # from montepython code https://github.com/brinckmann/montepython_public/blob/3.6/montepython/likelihood_class.py#L1096
-    ninst = sigma_T[i]**2 * \
-        np.exp(l_real*(l_real+1)*theta_planck[i]**2/(8*np.log(2))) #one over ninst
-    nis.append(1/ninst)
-ninst = np.array(nis).T
-ninst = np.sum(ninst, axis=1)
-noise = 1/ninst
-noise *= (2*np.pi)/(l_real*(l_real+1))
-"""plt.plot(l_real, noise*(l_real*(l_real+1))/(2*np.pi))
-plt.plot(l_real, p*(l_real*(l_real+1))/(2*np.pi))
-plt.xscale('log')
-plt.yscale('log')
-plt.show()
-sys.exit(1)"""
-
-pars = camb.CAMBparams()
-
-def likelihood(theta):
-    # camb stuff
-    #print(theta)
-    pars.set_cosmology(ombh2=theta[0], omch2=theta[1],
-                        tau=theta[3], cosmomc_theta=theta[2]/100,
-                        theta_H0_range=[5, 1000])
-    pars.InitPower.set_params(As=np.exp(theta[5])/10**10, ns=theta[4])
-    pars.set_for_lmax(2500, lens_potential_accuracy=0)
-    results = camb.get_background(pars) # computes evolution of background cosmology
-
-    cl = results.get_cmb_power_spectra(pars, CMB_unit='muK')['total'][:,0]
-    cl = np.interp(l_real, np.arange(len(cl)), cl)
-
-    cl *= (2*np.pi)/(l_real*(l_real+1)) # convert to C_l
-    
-    cl += noise
-    
-    x = (2*l_real + 1)* p/cl
-    L = -0.5*(-2*chi2(2*l_real+1).logpdf(x) 
-              - 2*np.log((2*l_real+1)/cl)).sum()
-    plt.plot(l_real, A*cl, label='{:.2f}'.format(L), ls='--')
-
-    return L, []
-
-A = (l_real*(l_real+1))/(2*np.pi)
-
-print(likelihood([0.022, 0.12, 1.04, 0.06, 0.96, 3.0]))
-print(likelihood([0.01146527, 0.09108639, 1.08655005, 0.1493919,  1.1031649,  3.30260607]))
-plt.plot(l_real, A*p)
-plt.plot(l_real, A*noise)
-plt.xlabel(r'$l$')
-plt.ylabel(r'$l(l+1)C_l/2\pi$')
-plt.legend()
-plt.title('Blue = Planck Best Fit\n Orange = Random Higher Likelihood')
-plt.tight_layout()
-plt.savefig('planck_bad_likelihood_with_cosmic_vairance_term.png')
-plt.show()
-sys.exit(1)
-
-file = 'Planck_chains_testing/'
+file = 'Planck_fit/'
 RESUME = False
 
 settings = PolyChordSettings(nDims, 0) #settings is an object
 settings.read_resume = RESUME
 settings.base_dir = file + '/'
-settings.nlive = 10
-settings.num_repeats = 2
+settings.nlive = 200*6
 
 output = pypolychord.run_polychord(likelihood, nDims, nDerived, settings, prior)
 paramnames = [('p%i' % i, r'\theta_%i' % i) for i in range(nDims)]
 output.make_paramnames_files(paramnames)
+
+from fgivenx import plot_contours, plot_lines
+import matplotlib.pyplot as plt
+from anesthetic import read_chains
+
+def signal():
+    def signal_func(_, parameters):
+        cl, sample = cmbs.get_samples(l, parameters, noise=planck_noise)
+        return sample
+    return signal_func
+
+fig, axes = plt.subplots(1)
+
+samples = read_chains('Planck_chains_testing/test')
+samples = samples.compress()
+
+names = ['p' + str(i) for i in range(6)]
+samples = samples[names].values
+plot_lines(signal, l, samples, axes, color='r')
+plt.plot(l, p, c='k', label='Planck')
+plt.xlabel(r'$l$')
+plt.ylabel(r'$C_l$')
+plt.legend()
+plt.tight_layout()
+plt.savefig('planck_fit.png', dpi=300)
+plt.show()
