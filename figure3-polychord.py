a66413e06b468f43b51452470ed7b90987752f0b
htjb <harrytjbevins@yahoo.co.uk>
2024-04-08 09:21:31 +0100

    only really need to run for three examples

--- figure3-polychord.py
+++ figure3-polychord.py
@@ -14,36 +14,31 @@ def signal_func_gen(freqs):
     return signal
 
 def signal_poly_prior(cube):
-    theta = np.zeros(4)
+    theta = np.zeros(3)
     theta[0] = UniformPrior(0, 4)(cube[0]) #amp
     theta[1] = UniformPrior(60, 90)(cube[1]) #nu_0
     theta[2] = UniformPrior(5, 40)(cube[2]) #w
-    theta[3] = LogUniformPrior(0.001, 0.1)(cube[3]) #noise
     return theta
 
 def joint_prior(cube):
-    theta = np.zeros(5)
+    theta = np.zeros(3)
     theta[0] = UniformPrior(0, 4)(cube[0]) #amp
     theta[1] = UniformPrior(60, 90)(cube[1]) #nu_0
     theta[2] = UniformPrior(5, 40)(cube[2]) #w
-    theta[3] = LogUniformPrior(0.001, 0.1)(cube[3]) #exp1 noise
-    theta[4] = LogUniformPrior(0.001, 0.1)(cube[4]) #exp2 noise
     return theta
 
 def exp1likelihood(theta):
     # gaussian log-likelihood
-    return (-0.5 * np.log(2*np.pi*theta[3]**2) \
-        - 0.5 * (exp1_data - exp1_sf([None], theta[:3]))**2/theta[3]**2).sum(),[]
+    return (-0.5 * np.log(2*np.pi*0.025**2) \
+        - 0.5 * (exp1_data - exp1_sf([None], theta))**2/0.025**2).sum(),[]
 
 def exp2likelihood(theta):
     # gaussian log-likelihood
-    return (-0.5 * np.log(2*np.pi*theta[3]**2) \
-        - 0.5 * (exp2_data - exp2_sf([None], theta[:3]))**2/theta[3]**2).sum(),[]
+    return (-0.5 * np.log(2*np.pi*0.025**2) \
+        - 0.5 * (exp2_data - exp2_sf([None], theta))**2/0.025**2).sum(),[]
 
 def jointlikelihood(theta):
-    exp1theta = theta[:4]
-    exp2theta = np.concatenate((theta[:3], theta[4:]))
-    return exp1likelihood(exp1theta)[0] + exp2likelihood(exp2theta)[0], []
+    return exp1likelihood(theta)[0] + exp2likelihood(theta)[0], []
 
 base = 'toy_chains_temp_sweep/'
 if not os.path.exists(base):
@@ -55,7 +50,7 @@ exp2_freq = np.linspace(80, 120, 100)
 exp1_sf = signal_func_gen(exp1_freq)
 exp2_sf = signal_func_gen(exp2_freq)
 
-temperatures = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
+temperatures = [0.15, 0.2, 0.25]
 true_params = np.array([0.2, 78.0, 10.0])
 
 try:
@@ -77,7 +72,7 @@ for t in temperatures:
             + np.random.normal(0, 0.025, 100)
         np.savetxt(base + f'exp2_data_{t}.txt', exp2_data)
 
-    run_poly(joint_prior, jointlikelihood, base + f'test_joint_{t}', nlive=1000, RESUME=RESUME, nDims=5)
+    run_poly(joint_prior, jointlikelihood, base + f'test_joint_{t}', nlive=1000, RESUME=RESUME)
     run_poly(signal_poly_prior, exp2likelihood, base + f'test_exp2_{t}', nlive=1000, RESUME=RESUME)
 
     exp2_samples = read_chains(base + f'test_exp2_{t}/test')
