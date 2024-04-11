bae029ff4d0fcb9776eff1f8e9e6a6eb3673ae87
htjb <harrytjbevins@yahoo.co.uk>
2024-04-11 15:04:04 +0100

    approximating hyp0f1 function

--- analytics.py
+++ analytics.py
@@ -34,17 +34,39 @@ plt.plot(lwmap, cltheory[0]*lwmap*(lwmap+1)/(2*np.pi))
 plt.ylim([0, 10000])
 plt.show()
 
+from scipy.special import loggamma, hyp0f1
+from numpy.linalg import slogdet
+from scipy.special import ive, iv
+
+def loghyp0f1(l, x):
+    """log(hyp0f1((2*l+1)/2, x**2))"""
+    ans1 = np.log(hyp0f1((2*l+1)/2, x**2))
+    with np.errstate(divide='ignore'):
+        ans2 = np.log(ive((2*l-1)/2,2*x)) + 2*x + loggamma((2*l+1)/2) -(2*l-1)/2*np.log(x)
+    ans3 = 2*x - l*np.log(x) + loggamma((2*l+1)/2) - np.log(4*np.pi)/2 
+    ans = ans1
+    ans = np.where(np.isfinite(ans), ans, ans2)
+    ans = np.where(np.isfinite(ans), ans, ans3)
+    return ans
+
 def logpdf(hatCF, hatCG, C, NF, NG, l):
     D = ((C+NF)*(C+NG) - C**2)/(2*l+1)
+    A = np.log(hyp0f1((2*l+1)/2, hatCF*hatCG*C**2/4/D**2))
+    B = loghyp0f1(l, np.sqrt(hatCF*hatCG)*C/2/D)
     logp = -2*loggamma((2*l+1)/2) - (2*l+1)/2*np.log(4*D/(2*l+1)) - \
-        ((C+NG)*hatCF + (C+NF)*hatCG)/(2*D) + (2*l-1)/2*np.log(hatCF*hatCG) + \
-            np.log(hyp0f1((2*l+1)/2, hatCF*hatCG*C**2/4/D**2))
-    return np.where(np.isfinite(logp), logp, -np.inf)
+        ((C+NG)*hatCF + (C+NF)*hatCG)/(2*D) + (2*l-1)/2*np.log(hatCF*hatCG)
+    return logp + A, logp + B
 
-print(logpdf(pobs[0], wobs[0], cltheory[0], pnoise, wnoise, lwmap))
-plt.plot(lwmap, (pobs[0] - wobs[0])*lwmap*(lwmap+1)/(2*np.pi))
+A, B = logpdf(pobs[0], wobs[0], cltheory[0], pnoise, wnoise, lwmap)
+
+plt.plot(lwmap, B, label='Approximation')
+plt.plot(lwmap, A, label='Scipy Function')
+plt.ylabel('logL')
+plt.xlabel('l')
+plt.legend()
+plt.savefig('logL_analytic_joint_likleihood.png',
+             dpi=300, bbox_inches='tight')
 plt.show()
-
 
 """C = 1
 NF = 0.1
