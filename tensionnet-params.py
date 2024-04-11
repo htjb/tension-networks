8629aed71c5ac381a95c0d6b6a5d080a84f2ea37
htjb <harrytjbevins@yahoo.co.uk>
2024-04-11 17:17:43 +0100

    using the median predicted logR sims for accuracy measure

--- tensionnet-params.py
+++ tensionnet-params.py
@@ -5,7 +5,10 @@ import matplotlib as mpl
 from matplotlib import rc
 from tensionnet.tensionnet import nre
 from scipy.stats import ecdf, norm
-from tensionnet.utils import calcualte_stats
+from tensionnet.utils import calcualte_stats, twentyone_example
+from tensionnet.robs import run_poly
+from anesthetic import read_chains
+import os
 
 # plotting stuff
 mpl.rcParams['axes.prop_cycle'] = mpl.cycler('color',
@@ -36,10 +39,13 @@ def signal_prior(n):
 exp1_freq = np.linspace(60, 90, 100)
 exp2_freq = np.linspace(80, 120, 100)
 
-exp2 = signal_func_gen(exp2_freq)
 exp1 = signal_func_gen(exp1_freq)
+exp2 = signal_func_gen(exp2_freq)
 
 base = 'parameter-sweep/'
+if not os.path.exists(base):
+    os.mkdir(base)
+RESUME_POLY = True
 
 lr = [1e-3, 1e-4]
 architecture = [[5]*5, [5]*10, [10]*5, [10]*25, [25]*5, 
@@ -56,8 +62,6 @@ activation = ['sigmoid', 'tanh', 'relu', 'softplus']
 import itertools
 iters = list(itertools.product(lr, architecture, activation))
 iters = list(map(list, iters))
-print(len(iters))
-print(iters)
 
 nsamp = 1000
 testing_data = None
@@ -72,7 +76,7 @@ for i, (lr, arch, act) in enumerate(iters):
                             arch, act)
         nrei.build_simulations(exp2, exp1, signal_prior, n=100000)
         model, data_test, labels_test = nrei.training(epochs=1000, 
-                                                        batch_size=5000)
+                                                        batch_size=1000)
         nrei.save(base + f'lr_{lr}_arch_{arch}_act_{act}.pkl')
 
     if testing_data is None:
@@ -81,11 +85,51 @@ for i, (lr, arch, act) in enumerate(iters):
         nrei.__call__(iters=testing_data)
     r = nrei.r_values
     mask = np.isfinite(r)
-    sigr = tf.keras.layers.Activation('sigmoid')(r[mask])
-    good_idx = sigr > 0.75
-    r = r[good_idx]
+    r = r[mask]
+    testing_data = testing_data[mask]
     print('Surviving Simulations: ', len(r)/nsamp*100, '%')
-    accuracy.append(len(r)/nsamp*100)
+
+    median_arg = np.argsort(r)[len(r)//2]
+    median_data = testing_data[median_arg]
+    median_exp2 = median_data[:len(exp2_freq)]*nrei.data_train.std(axis=0)[:len(exp2_freq)] \
+        + nrei.data_train.mean(axis=0)[:len(exp2_freq)]
+    median_exp1 = median_data[len(exp2_freq):]*nrei.data_train.std(axis=0)[len(exp2_freq):] \
+        + nrei.data_train.mean(axis=0)[len(exp2_freq):]
+
+    """plt.plot(exp2_freq, median_exp2, label='Exp2')
+    plt.plot(exp1_freq, median_exp1, label='Exp1')
+
+    plt.legend()
+    plt.show()
+    sys.exit(1)"""
+
+    signal_poly_prior, \
+        joint_prior, exp1likelihood, exp2likelihood, jointlikelihood = \
+        twentyone_example(median_exp1, median_exp2, exp1_freq, exp2_freq)
+    
+    run_poly(signal_poly_prior, exp1likelihood, base + 
+             f'exp1_median_lr_{lr}_arch_{arch}_act_{act}', 
+         nlive=100, RESUME=RESUME_POLY, nDims=4)
+    
+    run_poly(joint_prior, jointlikelihood, 
+             base + f'joint_median_lr_{lr}_arch_{arch}_act_{act}', 
+             nlive=125, RESUME=RESUME_POLY, nDims=5)
+    run_poly(signal_poly_prior, exp2likelihood, 
+             base + f'exp2_median_lr_{lr}_arch_{arch}_act_{act}', 
+             nlive=100, RESUME=RESUME_POLY, nDims=4)
+
+    exp1_samples = read_chains(base + 
+                    f'exp1_median_lr_{lr}_arch_{arch}_act_{act}/test')
+    exp2_samples = read_chains(base + 
+                    f'exp2_median_lr_{lr}_arch_{arch}_act_{act}/test')
+    joint_samples = read_chains(base + 
+                    f'joint_median_lr_{lr}_arch_{arch}_act_{act}/test')
+    
+    Rs = joint_samples.logZ(1000) - \
+        exp1_samples.logZ(1000) - exp2_samples.logZ(1000)
+
+    accuracy.append(np.abs(r[median_arg] - Rs.mean())/(r[median_arg] + Rs.mean())*2)
+    print('Accuracy: ', accuracy[-1], 'R: ', r[median_arg], 'Rs: ', Rs.mean())
 accuracy = np.array(accuracy)
 
 
@@ -98,7 +142,9 @@ ae3 = np.reshape(accuracy[:len(accuracy)//2], (len(architecture),
 ae4 = np.reshape(accuracy[len(accuracy)//2:], (len(architecture),
                                                         len(activation)))
 
-cb = ax[0].imshow(ae3, cmap='viridis', aspect='auto', vmin=0, vmax=100)
+cb = ax[0].imshow(ae3, cmap='viridis_r', aspect='auto', 
+                  vmin=0, vmax=np.max([np.max(ae3), np.max(ae4)]))
+plt.colorbar(cb, ax=ax[0], label=r'$\frac{|R - R_{obs}|}{R_{obs}}$')
 ax[0].set_xticks(range(len(activation)), activation, rotation=45)
 ax[0].set_yticks(range(len(connections)), 
                  ['{:.2f}'.format(np.log10(i)) for i in connections])
@@ -106,8 +152,9 @@ ax[0].set_xlabel('Activation Function')
 ax[0].set_ylabel(r'$\log_{10}($' + 'No. Weights' + r'$)$')
 ax[0].set_title('Learning Rate = 0.001')
 
-cb = ax[1].imshow(ae4, cmap='viridis', aspect='auto', vmin=0, vmax=100)
-plt.colorbar(cb, ax=ax[1], label='Percentage of correctly\nlabelled simulations')
+cb = ax[1].imshow(ae4, cmap='viridis_r', aspect='auto',
+                  vmin=0, vmax=np.max([np.max(ae3), np.max(ae4)]))
+plt.colorbar(cb, ax=ax[1], label=r'$\frac{|R - R_{obs}|}{R_{obs}}$')
 ax[1].set_xticks(range(len(activation)), activation, rotation=45)
 ax[1].set_yticks(range(len(connections)), 
                  ['{:.2f}'.format(np.log10(i)) for i in connections])
