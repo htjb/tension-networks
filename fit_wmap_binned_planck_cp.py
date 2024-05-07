f8ae1a50fc7b3bd1054a92a40c70c20c01a48a88
htjb <harrytjbevins@yahoo.co.uk>
2024-05-07 12:41:27 +0100

    fixing the planck fit wmap binned code

--- fit_wmap_binned_planck_cp.py
+++ fit_wmap_binned_planck_cp.py
@@ -4,6 +4,7 @@ from cmblike.data import get_data
 from cmblike.noise import planck_noise
 from cmblike.cmb import CMB
 import numpy as np
+import matplotlib.pyplot as plt 
 
 nDims = 5
 nDerived = 0
@@ -15,7 +16,13 @@ data_label = ''
 file = 'fit_wmap_binned_planck_cp/'
 _, lobs = get_data(base_dir='cosmology-data/').get_wmap()
 p = np.loadtxt('cosmology-data/planck_binned_like_wmap.txt')
+#p = p*(2*np.pi)/(lobs*(lobs+1))
 pnoise = planck_noise(lobs).calculate_noise()
+
+
+"""plt.plot(lobs, pnoise)
+plt.show()
+exit()"""
 
 parameters = ['omegabh2', 'omegach2', 'ns', 'As', 'h']
 prior_mins = [0.005, 0.08, 0.8, 2.6, 0.5]
@@ -23,10 +30,15 @@ prior_maxs = [0.04, 0.21, 1.2, 3.8, 0.9]
 
 cmbs = CMB(parameters=parameters, prior_mins=prior_mins, 
            prior_maxs=prior_maxs,
-           path_to_cp='/home/htjb2/rds/hpc-work/cosmopower')
+           path_to_cp='/Users/harrybevins/Documents/Software/cosmopower')
 
 likelihood = cmbs.get_likelihood(p, lobs, noise=pnoise, cp=True)
 prior = cmbs.prior
+
+"""print(prior(np.random.uniform(0, 1, nDims)))
+for i in range(10):
+    print(likelihood(prior(np.random.uniform(0, 1, nDims))))
+exit()"""
 
 settings = PolyChordSettings(nDims, 0) #settings is an object
 settings.read_resume = RESUME
