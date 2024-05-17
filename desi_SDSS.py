e285e376101a57379169d6b487712987baf6507e
htjb <harrytjbevins@yahoo.co.uk>
2024-05-17 14:42:42 +0100

    fiddling trying to get this working

--- desi_SDSS.py
+++ desi_SDSS.py
@@ -58,5 +58,131 @@ joint = read_chains('All_the_BAOs/DESI_SDSS/test')
 desi = read_chains('All_the_BAOs/DESI/test')
 sdss = read_chains('All_the_BAOs/SDSS/test')
 
-R = (joint.logZ - desi.logZ - sdss.logZ)
-print(R.mean(), R.std())
+R = (joint.logZ(1000) - desi.logZ(1000) - sdss.logZ(1000))
+R = R.mean()
+errorR = R.std()
+
+
+##############################################################################
+################################ Do NRE ######################################
+##############################################################################
+
+print('Running NRE...')
+nSamples = 50000
+load_data =True
+
+def nre_prior(N):
+    return np.array([np.random.uniform(prior_mins[i], prior_maxs[i], N) 
+                     for i in range(5)]).T
+
+def sdss_simulation(theta):
+    return np.concatenate(sdss_baos.get_sample(theta)[:2])
+
+def desi_simulation(theta):
+    return np.concatenate(desi_baos.get_sample(theta)[:2])
+
+from tensionnet.tensionnet import nre
+from scipy.stats import ecdf
+from tensionnet.utils import calcualte_stats
+from tensorflow.keras.optimizers.schedules import ExponentialDecay  
+
+lr = ExponentialDecay(1e-3, 1000, 0.9)
+nrei = nre(lr=lr)
+nrei.build_model(6+4, [6]*5, 'sigmoid', 
+                 skip_layers=False) 
+#nrei.build_compress_model(6, 4, [6, 6, 6, 2, 2], [5]*5,
+#        activation='sigmoid', compress='both',
+#        compress_layer_sizesB=[4, 4, 2, 2], use_bias=True, kernel_regularizer=None,)
+
+if load_data:
+    data_train1 = np.loadtxt('All_the_BAOs/data_train.txt')
+    data_test1 = np.loadtxt('All_the_BAOs/data_test.txt')
+    label_train1 = np.loadtxt('All_the_BAOs/labels_train.txt')
+    label_test1 = np.loadtxt('All_the_BAOs/labels_test.txt')
+    data_train2 = np.loadtxt('All_the_BAOs/data_train2.txt')
+    data_test2 = np.loadtxt('All_the_BAOs/data_test2.txt')
+    label_train2 = np.loadtxt('All_the_BAOs/labels_train2.txt')
+    label_test2 = np.loadtxt('All_the_BAOs/labels_test2.txt')
+
+    """data_train1_da = data_train1[:, ::2]
+    data_train1_dh = data_train1[:, 1::2]
+    data_test1_da = data_test1[:, ::2]
+    data_test1_dh = data_test1[:, 1::2]
+    data_train2_da = data_train2[:, ::2]
+    data_train2_dh = data_train2[:, 1::2]
+    data_test2_da = data_test2[:, ::2]
+    data_test2_dh = data_test2[:, 1::2]
+
+    data_train1 = data_train1_da**2*data_train1_dh
+    data_test1 = data_test1_da**2*data_test1_dh
+    data_train2 = data_train2_da**2*data_train2_dh
+    data_test2 = data_test2_da**2*data_test2_dh"""
+
+    sdss_redshift = np.concatenate([sdss_baos.d12[:, 0], sdss_baos.d16[:, 0]])
+    desi_redshift = np.concatenate([desi_baos.L1[:, 0], desi_baos.L2[:, 0]])
+    #print(sdss_redshift, desi_redshift)
+    redshifts = np.concatenate([sdss_redshift, desi_redshift])
+
+    """for i in range(100):
+        plt.plot(sdss_redshift, data_train1[i, :6], c='r', ls='', marker='o')
+        plt.plot(desi_redshift, data_train1[i, 6:], c='b', ls='', marker='o')
+    plt.show()
+    exit()"""
+    
+    nrei.data_train = np.concatenate([data_train1, data_train2])
+    nrei.data_test = np.concatenate([data_test1, data_test2])
+    nrei.labels_train = np.concatenate([label_train1, label_train2])
+    nrei.labels_test = np.concatenate([label_test1, label_test2])
+    nrei.simulation_func_A = sdss_simulation
+    nrei.simulation_func_B = desi_simulation
+    nrei.shared_prior = nre_prior
+    nrei.prior_function_A = None
+    nrei.prior_function_B = None
+else:
+    nrei.build_simulations(sdss_simulation, desi_simulation, 
+                        nre_prior, nSamples)
+    np.savetxt('All_the_BAOs/data_test2.txt', nrei.data_test)
+    np.savetxt('All_the_BAOs/data_train2.txt', nrei.data_train)
+    np.savetxt('All_the_BAOs/labels_test2.txt', nrei.labels_test)
+    np.savetxt('All_the_BAOs/labels_train2.txt', nrei.labels_train)
+
+nrei.training(epochs=1000, batch_size=10000)
+
+nrei.__call__(iters=1000)
+r = nrei.r_values
+mask = np.isfinite(r)
+
+fig, axes = plt.subplots(1, 2, figsize=(6.3, 4))
+axes[0].hist(r[mask], bins=25, density=True)
+axes[0].set_xlabel(r'$\log R$')
+axes[0].set_ylabel('Density')
+axes[0].axvline(R, ls='--', c='r')
+axes[0].axvspan(R - errorR, R + errorR, alpha=0.1, color='r')
+
+rsort  = np.sort(r[mask])
+c = ecdf(rsort)
+
+axes[1].plot(rsort, c.cdf.evaluate(rsort)) 
+axes[1].axhline(c.cdf.evaluate(R), ls='--',
+        color='r')
+axes[1].axhspan(c.cdf.evaluate(R - errorR), 
+        c.cdf.evaluate(R + errorR), 
+        alpha=0.1, 
+        color='r')
+axes[1].set_xlabel(r'$\log R$')
+axes[1].set_ylabel(r'$P(\log R < \log R^\prime)$')
+
+axes[1].axhline(c.cdf.evaluate(R), ls='--',
+            color='r')
+axes[1].axhspan(c.cdf.evaluate(R - errorR),
+            c.cdf.evaluate(R + errorR),
+            alpha=0.1,
+            color='r')
+
+stats = calcualte_stats(R, errorR, c)
+print(stats)
+
+plt.tight_layout()
+plt.savefig('All_the_BAOs/desi_sdss.pdf', bbox_inches='tight')
+plt.close()
+
